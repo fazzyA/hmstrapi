3393e938d2de895b34c2221ae431bc838643fbab
fazzyA <faz.pak@gmail.com>
2021-07-07 21:47:40 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2a1607a419bde44c2a1de731bf993fbc'),
+    },
+  },
+});
